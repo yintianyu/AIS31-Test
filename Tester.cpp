882d55fc445b94f04c012ee629374c01050f00a6
spike556 <spikexin@outlook.com>
2019-05-21 19:24:15 +0800

    random 64 bit

--- Tester.cpp
+++ Tester.cpp
@@ -7,16 +7,17 @@
 
 void Tester::generate(int quantity){
     std::random_device rd;
+    std::mt19937_64 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     numbers.clear();
     numbers.resize(quantity);
     for(int i = 0;i < quantity;i++){
-        numbers[i] = rd();
+        numbers[i] = gen() & 0xffffffffffff;
     }
 }
 
 bool Tester::test0(int quantity){
     std::set<TestNumber> s;
-    assert(quantity >= numbers.size());
+    assert(quantity <= numbers.size());
     for(int i = 0;i < quantity;i++){
         std::pair<std::set<TestNumber>::iterator, bool> p = s.insert(numbers[i]);
         if(!p.second){
